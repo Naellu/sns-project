cb0c70865f9f8936254cd94fb9c09574cae3d8d4
naellu <rsdf8156@naver.com>
2023-11-02 21:41:37 +0900

    프로젝트 파일 생성

---
+++ src/test/java/myproject/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package myproject.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
