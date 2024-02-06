2dbb385e79298f48f30ee296b242514060dc1c34
naellu <rsdf8156@naver.com>
2024-02-06 17:40:32 +0900

    [feat] 폰트 파일 추가 및 적용
    - Pretendard 폰트 적용

--- front/src/component/login/LoginPage.jsx
+++ front/src/component/login/LoginPage.jsx
@@ -19,7 +19,7 @@ const LoginPage = () => {
 
                     <div className="absolute w-[369px] h-[544px] top-0 left-0 border-[0.5px] border-solid border-[#a5a2a2]">
                         <div
-                            className="absolute w-[249px] h-[46px] top-[50px] left-[60px] [font-family:'Inter-Regular',Helvetica] font-normal text-black text-[40px] text-center tracking-[0] leading-[normal] ">NAELLU
+                            className="absolute w-[249px] h-[46px] top-[50px] left-[60px] [font-family:'Pretendard-Variable'] font-normal text-black text-[40px] text-center tracking-[0] leading-[normal] ">NAELLU
                         </div>
                         {/*<img*/}
                         {/*    className="absolute w-[249px] h-[86px] top-[40px] left-[60px] object-cover"*/}
@@ -27,16 +27,16 @@ const LoginPage = () => {
                         {/*/>*/}
 
                         <input
-                            className="absolute w-[249px] h-[44px] top-[153px] left-[60px] bg-[#F5F5F5] border-[0.1px] border-solid border-[#C9C9C9] [font-family:'Inter-Regular',Helvetica] font-normal text-[#8d8d8d] text-[12px] tracking-[0] leading-[normal] p-3"
+                            className="absolute w-[249px] h-[44px] top-[153px] left-[60px] bg-[#F5F5F5] border-[0.1px] border-solid border-[#C9C9C9] [font-family:'Pretendard-Variable'] font-normal text-[#8d8d8d] text-[12px] tracking-[0] leading-[normal] p-3"
                             placeholder={"아이디 또는 이메일"}/>
                         <input
-                            className="absolute w-[249px] h-[44px] top-[227px] left-[60px] bg-[#F5F5F5] border-[0.1px] border-solid border-[#C9C9C9] [font-family:'Inter-Regular',Helvetica] font-normal text-[#8d8d8d] text-[12px] tracking-[0] leading-[normal] p-3"
+                            className="absolute w-[249px] h-[44px] top-[227px] left-[60px] bg-[#F5F5F5] border-[0.1px] border-solid border-[#C9C9C9] [font-family:'Pretendard-Variable'] font-normal text-[#8d8d8d] text-[12px] tracking-[0] leading-[normal] p-3"
                             placeholder={"비밀번호"}/>
 
                         <div
                             className="absolute w-[249px] h-[39px] top-[304px] left-[60px] bg-[#abeaff] rounded-[10px]">
                             <button
-                                className="top-[10px] left-[105px] [font-family:'Inter-Bold',Helvetica] font-bold text-white text-[16px] whitespace-nowrap absolute tracking-[0] leading-[normal]"
+                                className="top-[10px] left-[105px] [font-family:'Pretendard-Variable'] font-bold text-white text-[16px] whitespace-nowrap absolute tracking-[0] leading-[normal]"
                                 onClick={clickLogEvent}>
                                 로그인
                             </button>
@@ -67,7 +67,7 @@ const LoginPage = () => {
                         />
 
                         <div
-                            className="h-[16px] top-[498px] left-[119px] [font-family:'Inter-Light',Helvetica] font-light text-[#acacac] text-[13px] absolute tracking-[0] leading-[normal]">
+                            className="h-[16px] top-[498px] left-[119px] [font-family:'Pretendard-Variable'] font-light text-[#acacac] text-[13px] absolute tracking-[0] leading-[normal]">
                             비밀번호를 잊으셨나요?
                         </div>
                     </div>
@@ -76,11 +76,11 @@ const LoginPage = () => {
                         className="absolute w-[369px] h-[80px] top-[564px] left-0 border-[0.5px] border-solid border-[#a4a2a2]">
                         <div className="relative w-[215px] h-[19px] top-[32px] left-[80px]">
                             <div
-                                className="h-[19px] -top-px left-0 [font-family:'Inter-Light',Helvetica] font-light text-black text-[16px] text-center whitespace-nowrap absolute tracking-[0] leading-[normal]">
+                                className="h-[19px] -top-px left-0 [font-family:'Pretendard-Variable'] font-light text-black text-[16px] text-center whitespace-nowrap absolute tracking-[0] leading-[normal]">
                                 계정이 없으신가요?
                             </div>
                             <div
-                                className="h-[19px] -top-px left-[141px] [font-family:'Inter-Bold',Helvetica] font-bold text-[#73aaea] text-[16px] text-center whitespace-nowrap absolute tracking-[0] leading-[normal]">
+                                className="h-[19px] -top-px left-[141px] [font-family:'Pretendard-Variable'] font-bold text-[#73aaea] text-[16px] text-center whitespace-nowrap absolute tracking-[0] leading-[normal]">
                                 가입하기
                             </div>
                         </div>
